876e627dda4cf999684c08e21c3d0300af4454ad
Melissa Morgan <mmorganwhiz@gmail.com>
2017-03-16 21:49:16 -0400

    second to last final

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-  var config = {
+ var config = {
     apiKey: "AIzaSyD_HsTZnyA-xAQqvB4mpEFSwwFHZPpRL7M",
     authDomain: "real-vs-fake-news.firebaseapp.com",
     databaseURL: "https://real-vs-fake-news.firebaseio.com",
     storageBucket: "real-vs-fake-news.appspot.com",
     messagingSenderId: "454901274668"
   };
-
  firebase.initializeApp(config);
-
-
  var database = firebase.database();
  console.log(database);
-
- 	var name = "";
-	var age = "";
-	var city = "";
-
-
+    var name = "";
+    var age = "";
+    var city = "";
   $("#startquiz").one("click", function(event){
-  	event.preventDefault();
+    event.preventDefault();
+    $("#dialog").dialog("close");
 
-  	//set the variables equal to the HTML Id's for inserting text
-  	name = $("#un").val().trim();
+    //set the variables equal to the HTML Id's for inserting text
+    name = $("#un").val().trim();
     age = $("#howold").val().trim();
     city = $("#unTwo").val().trim();
-
   
   database.ref().push({
-
         name: name,
         age: age,
         city: city,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
-
    
   database.ref().on("value", function(snapshot) {
       // storing the snapshot.val() in a variable for convenience
@@ -41,8 +33,8 @@
       
       // Getting an array of each key In the snapshot object
       var svArr = Object.keys(sv);
-		console.log("svARR");
-		console.log(svArr);
+        console.log("svARR");
+        console.log(svArr);
       
       // Finding the last user's key
       var lastIndex = svArr.length - 1;
@@ -50,32 +42,25 @@
       
       // Using the last user's key to access the last added user object
       var lastObj = sv[lastKey]
-
       // Console.logging the last user's data
       console.log(lastObj.name);
       console.log(lastObj.age);
       console.log(lastObj.city);
       // Change the HTML to reflect
-
-	// //Also when I refesh the page, it does not reset the values
+    // //Also when I refesh the page, it does not reset the values
  //      $("#para2").html(lastObj.name);
  //      $("#para3").html(lastObj.age);
  //      $("#para4").html(lastObj.city);
-
-
       // Handle the errors
     }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
-
     });
 });
-
-
   // Access webhose.io API to retreive News headlines from the onion and push to fakeNewsArray
 var Onion = {
   "async": true,
   "crossDomain": true,
-  "url": "https://webhose.io/search?token=fc70c82d-7bd6-4bed-aafe-4d4e65c7e5db&format=json&q=&site=theonion.com&size=12",
+  "url": "https://webhose.io/search?token=50a0a1fb-9dde-40e8-a606-06806e8399a4&format=json&q=&site=theonion.com&size=12",
   "dataType": "json",
   "method": "GET",
 }
@@ -89,8 +74,24 @@ $.ajax(Onion).done(function (response) {
   // Create arrays for real news titles and fake news titles
   var fakeNewsArray = [];
   var realNewsArray = [];
+  function stop() {
+        clearTimeout(processResults);
+      if (correctGuess > 5) {
+        $("#quizarea").html("<h2>" + "Game Over" + "</h2>" +"<br>" + "Winner  Winner");
+        questionNumber = 0;
+        correctGuess = 0;
+        incorrectGuess = 0;
+        
+    } else
+    {
+        $("#quizarea").html("<h2>" + "Game Over" + "</h2>" +"<br>" + "Looser  Looser");
+        questionNumber = 0;
+        correctGuess = 0;
+        incorrectGuess = 0;
+    };
+  }
  // Create a function which runs For Loop.  Remove suffix identifying news source.  Put edited title into appropriate array (real news or fake news)
-function getTitles(response, removesuffix, outputarray) {
+function getTitles(response, removesuffix, outputArray) {
   for (var i = 0; i < response.posts.length; i++) {
       var title = response.posts[i].title;
       // console.log("before:", title);
@@ -99,7 +100,7 @@ function getTitles(response, removesuffix, outputarray) {
         title = title.substring(0,suffixIndex);
       }
       if (title.length>0) {
-        outputarray.push(title);
+        outputArray.push(title);
         // console.log("after:", title);
       } else {
         // console.log("Rejected:", title);
@@ -111,7 +112,7 @@ function getTitles(response, removesuffix, outputarray) {
 var CNN = {
   "async": true,
   "crossDomain": true,
-  "url": "https://webhose.io/search?token=fc70c82d-7bd6-4bed-aafe-4d4e65c7e5db&format=json&q=language:english&site=cnn.com&size=12",
+  "url": "https://webhose.io/search?token=50a0a1fb-9dde-40e8-a606-06806e8399a4&format=json&q=language:english&site=cnn.com&size=12",
   "dataType": "json",
   "method": "GET",
 }
@@ -126,7 +127,7 @@ $.ajax(CNN).done(function (response) {
 var TMZ = {
   "async": true,
   "crossDomain": true,
-  "url": "https://webhose.io/search?token=fc70c82d-7bd6-4bed-aafe-4d4e65c7e5db&format=json&q=&site=tmz.com&size=12",
+  "url": "https://webhose.io/search?token=50a0a1fb-9dde-40e8-a606-06806e8399a4&format=json&q=&site=tmz.com&size=12",
   "dataType": "json",
   "method": "GET",
 }
@@ -162,8 +163,11 @@ function processResults () {
     questionNumber++;
     console.log(questionNumber);
   }
+   if (questionNumber === 10) {
+      stop();
     
   };
+};
   // Enable on click event listeners
   // If REAL button is clicked
     $( "#real_button" ).click(function() {
@@ -173,7 +177,7 @@ function processResults () {
         console.log(incorrectGuess);
         $("#rightguess").html("Correct Guesses: " + correctGuess);
         $("#wrongguess").html("Wrong Guesses: " + incorrectGuess);
-        setTimeout(function(){processResults()}, 3000)
+        setTimeout(function(){processResults()}, 500)
       }
       else {
         $("#quizoutput").html("Yes!  This is TRUE")
@@ -181,7 +185,7 @@ function processResults () {
         console.log(correctGuess);
         $("#rightguess").html("Correct Guesses: " + correctGuess);
         $("#wrongguess").html("Wrong Guesses: " + incorrectGuess);
-        setTimeout(function(){processResults()}, 3000)
+        setTimeout(function(){processResults()}, 500)
       };
     });
     // If FAKE button is clicked
@@ -191,20 +195,15 @@ function processResults () {
       correctGuess++;
       $("#rightguess").html("Correct Guesses: " + correctGuess);
       $("#wrongguess").html("Wrong Guesses: " + incorrectGuess);
-      setTimeout(function(){processResults()}, 3000)
+      setTimeout(function(){processResults()}, 500)
     } 
       else {
       $("#quizoutput").html("Sorry!  This is True");
       incorrectGuess++;
       $("#rightguess").html("Correct Guesses: " + correctGuess);
       $("#wrongguess").html("Wrong Guesses: " + incorrectGuess);
-      setTimeout(function(){processResults()}, 3000)
+      setTimeout(function(){processResults()}, 500)
     };
     });
     // After 10 questions, end the quiz
-    if (questionNumber = 10) {
-        console.log("Game Over")
-    } else
-    {
-        console.log("Continue")
-    };
+  
